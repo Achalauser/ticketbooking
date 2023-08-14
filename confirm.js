eb82fff484c7ccc1c2443467de39eed90ff78be6
Achalauser <141643033+Achalauser@users.noreply.github.com>
2023-08-14 11:01:38 +0530

    Add files via upload

---
+++ confirm.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    const summaryTableHTML = localStorage.getItem('summaryTableHTML');
+
+// Display the summaryTableHTML in the #summary-table div
+document.getElementById('summary-table').innerHTML = summaryTableHTML;
+    // Retrieve stored values from local storage
+    const fullName = localStorage.getItem('fullName');
+    const mobileNumber = localStorage.getItem('mobileNumber');
+    const email = localStorage.getItem('email');
+    const gender = localStorage.getItem('gender');
+    const countryCode = localStorage.getItem('countryCode'); 
+    
+
+    
+  
+   
+    
+  
+    // Populate the order summary
+    document.getElementById('name').textContent = fullName;
+    document.getElementById('mobile').textContent = `${countryCode} ${mobileNumber}`; 
+    document.getElementById('email').textContent = email;
+    document.getElementById('gender').textContent = gender;
+  
+   
+  };
+  
+  
